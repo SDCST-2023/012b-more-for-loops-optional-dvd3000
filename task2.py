08861ccb44576a5b3b284b68a3a3aaba9cb4ec9d
dvd3000 <patch12j@gmail.com>
2023-03-29 11:39:50 -0700

    edit

--- task2.py
+++ task2.py
@@ -7,5 +7,21 @@ If they are not correct, the program will say "Access Denied".
 There will be a maximum of 3 guesses allowed
 '''
 
+
 expectedUsername = "systemadmin"
 expectedPassword = "master"
+
+
+u = input("Enter Username: ")
+
+if u == expectedUsername:
+    for i in range(3,0,-1):
+        p = input(f"({i}) Enter Password: ")
+        if p == expectedPassword:
+            print(f"Welcome, {expectedUsername}!")
+            break
+        else:
+            print(f"Wrong Password. Idiot.")
+    print("You died.")
+else:
+    print(f"User \"{u}\" not found!")
--- task3.py
+++ task3.py
@@ -12,3 +12,27 @@ etc.
 
 users = ["annie","betty","charles","doug","eddie","flon"]
 passwords = ["12345","password","iloveyou","mom","default","0"]
+
+while True:
+    u = input("\nEnter Username: ")
+    if u in users:
+        n = users.index(u)
+        for i in range(3,0,-1):
+            p = input(f"{u} ({n}), Enter Password ({i}): ")
+            if p == passwords[n]:
+                print(f"Welcome, {u}!")
+                break
+            elif p == "quit":
+                print("Goodbye!")
+                break
+        else:
+            print(f"Wrong Password for {u}. Try again? Or type \"quit\" to exit.")
+            continue
+    if u == "quit":
+        print("Goodbye!")
+        break
+    elif u not in users:
+        print(f"User \"{u}\" not found. Try again? Or type \"quit\" to exit.")
+        continue
+    else:
+        break
